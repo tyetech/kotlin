b4cfdae0be10c9acfd9dfbc33bbf7e8881cb13e0
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-08-24 15:19:42 +0300

    Forbid suspend functions annotated with @kotlin.test.Test
     #KT-26200 Fixed

---
+++ compiler/testData/diagnostics/testsWithStdLib/coroutines/suspendTest.kt
@@ -0,0 +1,19 @@
+// FILE: test.kt
+
+package kotlin.test
+
+annotation class IrrelevantClass
+
+public typealias Test = IrrelevantClass
+
+// FILE: main.kt
+
+import kotlin.test.Test
+
+class A {
+    @Test
+    <!UNSUPPORTED!>suspend<!> fun test() {}
+}
+
+@Test
+<!UNSUPPORTED!>suspend<!> fun test() {}
