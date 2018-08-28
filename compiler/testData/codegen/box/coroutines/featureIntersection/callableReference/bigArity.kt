62a2c68b65a89402b9c053ee6f0fd9a7ae38b948
Ilmir Usmanov <ilmir.usmanov@jetbrains.com>
2018-08-28 21:47:22 +0300

    Add test for callable references to suspend functions with big arity
     #KT-24854: Fixed

---
+++ compiler/testData/codegen/box/coroutines/featureIntersection/callableReference/bigArity.kt
@@ -0,0 +1,31 @@
+// !LANGUAGE: +ReleaseCoroutines
+// IGNORE_BACKEND: JS_IR, JS, NATIVE
+// WITH_REFLECT
+// WITH_COROUTINES
+
+import kotlin.reflect.full.callSuspend
+import helpers.*
+import kotlin.coroutines.*
+
+class A {
+    suspend fun foo(
+        p00: A, p01: A, p02: A, p03: A, p04: A, p05: A, p06: A, p07: A, p08: A, p09: A,
+        p10: A, p11: A, p12: A, p13: A, p14: A, p15: A, p16: A, p17: A, p18: A, p19: A,
+        p20: A, p21: A, p22: A, p23: A, p24: A, p25: A, p26: A, p27: A, p28: A, p29: String
+    ): String {
+        return p29
+    }
+}
+
+fun builder(c: suspend () -> Unit) {
+    c.startCoroutine(EmptyContinuation)
+}
+
+fun box(): String {
+    val a = A()
+    var res = "FAIL"
+    builder {
+        res = A::foo.callSuspend(a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, "OK")
+    }
+    return res
+}
